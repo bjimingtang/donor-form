ca1de192cf3581fc901d4f18c691a11850724e35
bjimingtang <brendan.jiming.tang@gmail.com>
2021-05-20 15:26:20 -0700

    insertion prepared statement written

--- js/confirmation.js
+++ js/confirmation.js
@@ -6,7 +6,12 @@ function confirmSubmission(fileName){
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      window.location.href = '../index/thankyou.php';
+      if (this.responseText == "success") {
+        window.location.href = '../index/thankyou.php';
+      } else {
+        alert("Data insertion failed! Please try again.");
+      }
+
     }
   };
   xhttp.open("POST", "../php/confirmSubmission.php", true);
--- php/confirmSubmission.php
+++ php/confirmSubmission.php
@@ -3,23 +3,20 @@
     session_start();
     require_once 'database.php';
 
-    // store the user info in MySQL
-    // $_SESSION["fname"] = $_POST["fname"];
-    // $_SESSION["lname"] = $_POST["lname"];
-    // $_SESSION["address"] = $_POST["address"];
-    // $_SESSION["city"] = $_POST["city"];
-    // $_SESSION["state"] = $_POST["state"];
-    // $_SESSION["country"] = $_POST["country"];
-    // $_SESSION["postcode"] = $_POST["postcode"];
-    // $_SESSION["phone"] = $_POST["phone"];
-    // $_SESSION["email"] = $_POST["email"];
-    // $_SESSION["contact"] = $_POST["contact"];
-    // $_SESSION["payment"] = $_POST["payment"];
-    // $_SESSION["frequency"] = $_POST["frequency"];
-    // $_SESSION["amount"] = $_POST["amount"];
-    // $_SESSION["comments"] = $_POST["comments"];
-
-    
+    // store the user info in MySQL with a prepared statement
+    $stmt = $mysqli->prepare("CALL donationdb.insertDonor(?,?,?,?,?,?,?,?,?,?,?,?,?,?,@successConfirmation)");
+    $stmt->bind_param("ssssssssssssds", $_SESSION["fname"], $_SESSION["lname"], $_SESSION["address"], $_SESSION["city"],
+      $_SESSION["state"], $_SESSION["country"], $_SESSION["postcode"], $_SESSION["phone"],
+      $_SESSION["email"], $_SESSION["contact"], $_SESSION["payment"], $_SESSION["frequency"],
+      $_SESSION["amount"], $_SESSION["comments"]);
+    $stmt->execute();
+    $result = $stmt->fetch();
+    $successConfirmation = $result['successConfirmation'];
+    if ($successConfirmation > 0) {
+      echo "success";
+    } else {
+      echo "fail";
+    }
   }
   catch(Exception $e) {
     echo 'Message: ' . $e->getMessage();
